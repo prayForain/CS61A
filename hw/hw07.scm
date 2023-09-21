72dca05b8e956935dbd0ac380f05c811de68aeee
Gamift <102781335+prayForain@users.noreply.github.com>
2023-09-21 16:18:18 +0800

    Add files via upload

---
+++ hw/hw07.scm
@@ -0,0 +1,59 @@
+(define (filter-lst fn lst)
+  (if (null? lst)
+      nil
+      (if (fn (car lst))
+      (cons (car lst) (filter-lst fn (cdr lst)))
+      (filter-lst fn (cdr lst))))
+)
+
+;;; Tests
+(define (even? x)
+  (= (modulo x 2) 0))
+(filter-lst even? '(0 1 1 2 3 5 8))
+; expect (0 2 8)
+
+
+(define (interleave first second)
+  'YOUR-CODE-HERE
+  (if (null? first)
+      second
+      (if (null? second)
+          first
+          (cons (car first)
+                (cons (car second)
+                      (interleave (cdr first)
+                                  (cdr second))))))
+)
+
+(interleave (list 1 3 5) (list 2 4 6))
+; expect (1 2 3 4 5 6)
+
+(interleave (list 1 3 5) nil)
+; expect (1 3 5)
+
+(interleave (list 1 3 5) (list 2 4))
+; expect (1 2 3 4 5)
+
+
+(define (accumulate combiner start n term)
+  'YOUR-CODE-HERE
+  (if (= n 0)
+      start
+      (combiner (term n)
+                (accumulate combiner
+                            start
+                            (- n 1)
+                            term)))
+)
+
+
+(define (no-repeats lst)
+  'YOUR-CODE-HERE
+  (if (null? lst)
+      nil
+      (cons (car lst)
+            (filter-lst (lambda (item)
+                          (not (= (car lst) item)))
+                        (no-repeats (cdr lst)))))
+)
+
