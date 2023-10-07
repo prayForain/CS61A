9ea6cc4e9c8c65a273167a59b68514a88d9894e4
Gamift <102781335+prayForain@users.noreply.github.com>
2023-10-07 20:09:31 +0800

    Add files via upload

---
+++ lab/lab14.scm
@@ -0,0 +1,20 @@
+#lang racket
+(define nil '())
+(define (split-at lst n)
+  'YOUR-CODE-HERE
+  (if (equal? (cdr lst) nil)
+      (cons (list (car lst)) (cdr lst))
+      (if (= n 0)
+          (cons nil lst)
+          (cons (cons (car lst) (car (split-at (cdr lst) (- n 1))))
+                (cdr (split-at (cdr lst) (- n 1))))))
+)
+
+
+(define (compose-all funcs)
+  'YOUR-CODE-HERE
+  (if (equal? funcs nil)
+      (lambda (x) x)
+      (lambda (x) ((compose-all (cdr funcs)) ((car funcs) x))))
+)
+
